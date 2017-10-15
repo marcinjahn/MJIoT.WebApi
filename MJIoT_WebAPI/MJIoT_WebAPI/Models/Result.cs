a7b72b055b489dde24cd365714dcbf85d0a5b046
Marcin Jahn <jahnmarcin@outlook.com>
2017-10-15 20:27:49 +0200

    created response model for devices request

---
+++ MJIoT_WebAPI/MJIoT_WebAPI/Models/Result.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MJIoT_WebAPI.Models
+{
+    public class DevicesResult
+    {
+        public List<DeviceResult> Devices { get; set; }
+    }
+
+    public class DeviceResult
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }  //sender lub listener lub bidirectional
+        public bool IsConnected { get; set; }
+        public List<string> ConnectedDevices { get; set; }
+        //przydałby się jeszcze obrazek
+    }
+}
